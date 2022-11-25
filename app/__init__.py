1a82873e3d2d71251086aa7f4bef63905e878f70
gallopelado <juanftp100@gmail.com>
2022-11-25 19:59:23 -0300

    add: Cambios hechos en la base del proyecto * Cambios en la base del html * revisamos los endpoints de ciudad * revisamos el menu * corregimos los recursos de javascript, css

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-from app import rutas
+#from app import rutas
 # Vamos a importar lo necesario para los referenciales
 from app.rutas.ciudad.ciudad_routes import ciudadmod
 
--- app/rutas/ciudad/ciudad_routes.py
+++ app/rutas/ciudad/ciudad_routes.py
@@ -3,10 +3,15 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash,
 # Se modularizan las vistas de ciudades.
 ciudadmod = Blueprint("ciudad", __name__, template_folder="templates")
 
+# Lugarcito de los endpoints
 @ciudadmod.route('/')
 def index():
     #return "Hola mundo en ciudad"
     return render_template('ciudad/index.html')
+
+@ciudadmod.route('/listar_todos')
+def listar_todos():
+    return 'Aqui voy a listar mis ciudades'
 
 
 
