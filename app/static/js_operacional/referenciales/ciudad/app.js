fdb1f7a2ac49260f9743af33da435095a3e94866
gallopelado <juanftp100@gmail.com>
2022-12-14 23:02:22 -0300

    add: configurando botón editar

--- app/static/js_operacional/referenciales/ciudad/app.js
+++ app/static/js_operacional/referenciales/ciudad/app.js
@@ -15,7 +15,25 @@ $(async()=> {
 
     $('button[name="bt_ciudad_eliminar"]').on('click', () => {
       alert('Soy la forma mas larga de usarme')
+      //funcion eliminar
+    })
+
+    // boton editar
+    $('button[name="bt_ciudad_editar"]').on('click', function() {
       //funcion editar
+      //console.log($(this).attr('id_ciudad'), $(this).attr('descri_ciudad'))
+      const ciudad = { 
+        id: $(this).attr('id_ciudad') 
+        ,descripcion: $(this).attr('descri_ciudad')
+      }
+
+      $('#btn_nacionalidad_enviar').text('Editar').removeClass('btn-primary').addClass('btn-warning')
+      //$('#btn_nacionalidad_enviar').removeClass('btn-primary')
+      //$('#btn_nacionalidad_enviar').addClass('btn-warning')
+      // seteando el modal
+      $('#txt_id_ciudad').val(ciudad.id)
+      $('#txt_ciudad').val(ciudad.descripcion)
+      modal_ciudad_formulario.modal('show')
     })
 
     // asignando el evento al boton agregar
@@ -96,10 +114,12 @@ $(async()=> {
   }
 })
 
-window.editar_ciudad = (id, texto) => {
+// otra forma de trabajar el editar
+/* window.editar_ciudad = (id, texto) => {
   alert(`editando elemento, ${id} - ${texto}`)
   //editar
-}
+} */
+// onclick="editar_ciudad('${item.id}', '${item.descripcion}')"
 
 const traerCiudades = async() => {
   try{
@@ -109,11 +129,15 @@ const traerCiudades = async() => {
     const tbody = $("#tbody_ciudad");
     let contenido = "";
     lista_ciudades.forEach((item) => {
-      contenido += `<tr>
-                        <td>${item.descripcion}</td>
+      contenido += `<tr name="mi_tr">
+                        <td>
+                          <a href="">
+                            ${item.descripcion}
+                          </a>
+                          </td>
                         <td class="d-flex justify-content-end">
                             <div class="btn-group" role="group" aria-label="Botones para accion">
-                                <button type="button" name="bt_ciudad_editar" class="btn btn-outline-primary" onclick="editar_ciudad('${item.id}', '${item.descripcion}')">Editar</button>
+                                <button type="button" name="bt_ciudad_editar" class="btn btn-outline-primary" id_ciudad="${item.id}" descri_ciudad="${item.descripcion}">Editar</button>
                                 <button type="button" name="bt_ciudad_eliminar" class="btn btn-outline-danger">Eliminar</button>
                             </div>
                         </td>
